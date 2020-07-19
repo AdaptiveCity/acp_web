e8740e3f3a52a5cd8db3393979c3b43eca641044
Ian Lewis <ijl20@cam.ac.uk>
2020-07-19 09:57:59 +0100

    sensor/chart WIP - d3 replaces Chart.js

--- cdbb/sensor/static/sensor/js/time_chart.js
+++ cdbb/sensor/static/sensor/js/time_chart.js
@@ -1,15 +1,57 @@
 "use strict"
-// uses API_READINGS, API_SENSORS, SENSOR_REALTIME set in template
+// uses ACP_IP, API_READINGS, API_SENSORS, SENSOR_REALTIME set in template
+
+//********************************************************************************
+//***********  LOAD VARS FROM SERVER    ******************************************
+//********************************************************************************
+
+var YYYY = '2020'; //{{config_yyyy}}; // Year e.g. 2016
+var MM = '07'; //{{config_MM}};     // Month e.g. 07 = July
+var DD = '17'; //{{config_dd}};     // day e.g. 23
+
+var YYYYMMDD = YYYY+'/'+MM+'/'+DD; //'{{config_yyyy}}/{{config_MM}}/{{config_dd}}';
+
+var feed_id = 'mqtt_acp'; //''{{config_feed_id}}';
+
+var plot_date; // date of currently displayed plot (initialized from YYYY/MM/DD)
+
+var plot_date_minus_1; // plot_date - 1 day
+var plot_date_plus_1; // plot_date + 1 day
+
+var month_of_year = new Array("spacer", "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+var day_of_week = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+
+// d3 scatterplot parameters
+var chart_svg;    // chart svg element
+var chart_xScale; // d3 scale fn for x axis
+var chart_xAxis;  // x axis
+var chart_xValue; // value for x axis selected from current data object (i.e. timestamp)
+var chart_xMap;   // fn for x display value
+var chart_yScale;
+var chart_yAxis;
+var chart_yValue;
+var chart_yMap;
+//var chart_cValue; // value from current data point to determine color of circle (i.e. route_id)
+var chart_color;  // color chosen for current circle on scatterplaot
+var chart_tooltip;
+
+// time of day for scatterplot to start/end
+var CHART_START_TIME = 0; // start chart at midnight
+var CHART_END_TIME = 24;  // end chart at midnight
+
+var CHART_DOT_RADIUS = 6; // size of dots on scatterplot
+var CHART_Y_MAX = 40; // FIX limit of Y axis
+
+var feature_label = "Temperature (celcius)"; //DEBUG get from metadata
 
 var loading_el; // loading element animated gif
-var acp_id = "ijl20-sodaq-ttn";
 var readings_date = new Date().toISOString().split('T')[0];
 var readings_source = "mqtt_acp";
 var readings_feature = "temperature";
 var today = new Date().toISOString().split('T')[0];
 
 // Called on page load
-function chart_init() {
+function init() {
     loading_el = document.getElementById('loading');
 
    var form_date = document.getElementById("form_date");
@@ -23,13 +65,29 @@ function chart_init() {
 
    document.getElementById("feature").value = readings_feature;
 
+   plot_date = new Date(YYYY,MM-1,DD); // as loaded in page template config_ values;
+
+   var today = new Date();
+
+   // test if YYYY/MM/DD is TODAY'S DATE
+   if (today.getFullYear()==YYYY && today.getMonth()==(MM-1) && today.getDate()==DD)
+   {
+     console.log('plotting today - reloading in 6 minutes');
+     setTimeout( function () { location.reload(); }, 6 * 60 * 1000 );
+   }
+
+   // set up layout / axes of scatterplot
+   init_chart();
+
    get_readings();
+
 }
+
 
 // Use API_READINGS to retrieve requested sensor readings
 function get_readings() {
     var readings_url = API_READINGS + 'historicaldata/'
-                        + '?sensor=' + acp_id
+                        + '?sensor=' + ACP_ID
                         + '&date=' + readings_date
                         + '&source=' + readings_source
                         + '&feature=' + readings_feature;
@@ -50,81 +108,7 @@ function handle_readings(results) {
 
     loading_el.className = 'loading_hide'; // set style "display: none"
 
-    // Split timestamp and data into separate arrays
-    var labels = [],
-        readings=[],
-        rdate = results["date"],
-        rsensor = results["sensor"],
-        rfeature = results["feature"];
-
-    results["data"].forEach(function(packet) {
-      var date = new Date(parseFloat(packet.ts) * 1000);
-      var hours = date.getHours();
-      labels.push(hours)
-      readings.push(parseFloat(packet.val));
-    });
-
-    // // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("chart_canvas");
-
-    // Create the chart.js data structure using 'labels' and 'readings'
-    var tempData = {
-      labels : labels,
-      datasets : [{
-          fillColor             : "rgba(151,187,205,0.2)",
-          strokeColor           : "rgba(151,187,205,1)",
-          pointColor            : "rgba(151,187,205,1)",
-          pointStrokeColor      : "#fff",
-          pointHighlightFill    : "#fff",
-          pointHighlightStroke  : "rgba(151,187,205,1)",
-          data                  : readings
-      }]
-    };
-
-    var opts = {
-      animation: false,
-      title: {
-        display: true,
-        text: 'Date: ' + rdate+', Sensor: '+rsensor+', Feature: '+rfeature,
-        fontSize: 15
-
-      },
-      fill: false,
-      showLines: false,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes : [{
-             gridLines : {
-              display : false,
-             },
-             ticks: {
-              autoSkip: true,
-              maxTicksLimit: 20.1
-             },
-             scaleLabel: {
-              display: true,
-              labelString: 'Hour of the day',
-              fontStyle: 'bold',
-              fontSize: 13
-             }
-        }]
-      }
-    }
-
-    // // // Instantiate a new chart
-    var myNewChart = new Chart(ctx , {
-      type: "line",
-      data: tempData,
-      options: opts
-    });
-
-    if(readings_date == today) {
-        console.log("plot_realtime() will be called");
-        //debug plot_realtime() disabled
-        //plotRealTime();
-    }
+    draw_chart(results["data"]);
 }
 
 // Subscribe to RTMonitor to receive incremental data points and update chart
@@ -181,4 +165,268 @@ function plot_realtime() {
         myNewChart.update();
       }
     };
+}
+
+// ******************************************
+// Initialize the chart to appear on the page
+// - not yet with any data
+// ******************************************
+function init_chart()
+{
+      // get the height and width of the "chart" div, and set d3 svg element to that
+      var svg_width = document.getElementById("chart").clientWidth;
+      var svg_height = document.getElementById("chart").clientHeight;
+
+      // calculate the dimensions of the actual chart within the SVG area (i.e. allowing margins for axis info)
+      var chart_width = svg_width - 210;
+      var chart_height = svg_height - 110;
+      var chart_offsetx = 60;
+      var chart_offsety = 20;
+
+    // add the graph canvas to the body of the webpage
+    chart_svg = d3.select("#chart").append("svg")
+        .attr("width", svg_width)
+        .attr("height", svg_height)
+        .attr("class", "plot_svg")
+        .append("g")
+        .attr("transform", "translate(" + chart_offsetx + "," + chart_offsety + ")");
+
+    // setup x axis
+    chart_xScale = d3.scaleTime().range([0, chart_width]); // value -> display
+
+    var tick_count = 24;
+    if (chart_width < 600)
+        tick_count = 12;
+
+    chart_xAxis = d3.axisBottom().scale(chart_xScale).ticks(tick_count);
+
+    // setup y axis
+    chart_yScale = d3.scaleLinear().range([chart_height, 0]); // value -> display
+    chart_yAxis = d3.axisLeft().scale(chart_yScale).ticks(10);
+
+    // setup chart functions where d = the zone transit record
+    chart_xValue = function(d) { return make_date(d.ts);}; // data -> value
+    chart_xMap = function(d) { return chart_xScale(chart_xValue(d));}; // data -> display
+
+    //DEBUG the location of the charted y value should be in metadata
+    chart_yValue = function(d) { return d.val;}, // data -> value
+    chart_yMap = function(d) { return chart_yScale(Math.min(chart_yValue(d), CHART_Y_MAX));}; // data -> display
+
+    // setup fill color
+    //chart_cValue = function(d) { return d.route_id; },
+    chart_color = function (d) {
+        if (d.duration > CHART_Y_MAX)
+        {
+          return 'red';
+        }
+        return (d.route_id==null || d.route_id=="") ? 'green' : 'yellow';
+    };
+
+    // initialize x axis - will redraw when data received
+    var min_date = new Date(YYYY,MM-1,DD);
+    min_date.setHours(CHART_START_TIME);
+    min_date.setMinutes(0);
+    min_date.setSeconds(0);
+
+    var max_date = new Date(min_date);
+    max_date.setHours(CHART_END_TIME);
+
+    chart_xScale.domain([min_date, max_date]);
+    chart_yScale.domain([0, CHART_Y_MAX]);
+
+    // x-axis
+    chart_svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + chart_height + ")")
+      .call(chart_xAxis
+            .tickSize(-chart_height, 0, 0)
+            .tickFormat(d3.timeFormat("%H"))
+       )
+      .attr("class", "grid")
+      .append("text")
+      .attr("class", "label")
+      .attr("x", chart_width)
+      .attr("y", 26)
+      .style("text-anchor", "end")
+      .text("Time of day");
+
+    // y-axis
+    chart_svg.append("g")
+      .attr("class", "y axis")
+      .call(chart_yAxis
+            .tickSize(-chart_width, 0, 0)
+            //.tickFormat("")
+      )
+      .attr("class", "grid")
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -50)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text(feature_label);
+
+    // add the tooltip area to the webpage
+    chart_tooltip = d3.select("#chart").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+}
+
+// ****************************************************************************
+// *********  Update the chart with transit data        ***********************
+// ****************************************************************************
+function draw_chart(readings)
+{
+    console.log('Drawing chart size='+readings.length, readings);
+
+    // do nothing if no data is available
+    if (readings.length == 0) return;
+
+    /*
+     * value accessor - returns the value to encode for a given data object.
+     * scale - maps value to a visual display encoding, such as a pixel position.
+     * map function - maps from data value to display value
+     * axis - sets up axis
+    */
+
+      // don't want dots overlapping axis, so add in buffer to data domain
+      var min_date = d3.min(readings, chart_xValue);
+      min_date.setHours(CHART_START_TIME);
+      min_date.setMinutes(0);
+      min_date.setSeconds(0);
+
+      var max_date = new Date(min_date);
+      max_date.setHours(CHART_END_TIME);
+
+      chart_xScale.domain([min_date, max_date]);
+      // chart_yScale.domain([0, d3.max(readings, chart_yValue)+1]);
+      //DEBUG y-scale hardcoded to CHART_Y_MAX so charts are comparable
+      chart_yScale.domain([0, CHART_Y_MAX]);
+
+      chart_svg.select(".x.axis").call(chart_xAxis);
+      chart_svg.select(".y.axis").call(chart_yAxis);
+
+      chart_svg.selectAll(".dot")
+          .remove();
+
+      // draw dots
+      chart_svg.selectAll(".dot")
+          .data(readings)
+          .enter().append("circle")
+          .attr("class", "dot")
+          .attr("r", CHART_DOT_RADIUS)
+          .attr("cx", chart_xMap)
+          .attr("cy", chart_yMap)
+          .style("fill", function(d) { return chart_color(d); })
+          .on("mouseover", function(d) {
+              chart_tooltip.transition()
+                   .duration(500)
+                   .style("opacity", 0);
+              chart_tooltip.transition()
+                   .duration(200)
+                   .style("opacity", .9);
+              chart_tooltip.html(tooltip_html(d))
+                   .style("left", (d3.event.pageX + 5) + "px")
+                   .style("top", (d3.event.pageY - 28) + "px");
+          })
+          .on("mouseout", function(d) {
+              chart_tooltip.transition()
+                   .duration(500)
+                   .style("opacity", 0);
+          });
+
+      // add text for latest datapoint
+
+      var p = readings[readings.length - 1];
+
+      chart_svg.append("svg:rect")
+          .attr('x', chart_xMap(p)+CHART_DOT_RADIUS+4)
+          .attr('y', chart_yMap(p)-27)
+          .attr('width', 140)
+          .attr('height', 36)
+          .attr('rx', 6)
+          .attr('ry', 6)
+          .style('fill', 'white')
+
+      var p_time = make_date(p.ts);
+      var p_time_str = ' @ '+('0'+p_time.getHours()).slice(-2)+':'+('0'+p_time.getMinutes()).slice(-2);
+      chart_svg.append("svg:text")
+          .attr('x', chart_xMap(p))
+          .attr('y', chart_yMap(p))
+          .attr('dx', CHART_DOT_RADIUS+10)
+          .style('font-size', '22px')
+          .style('fill', '#333')
+          //DEBUG property name for chart should be in config metadata
+          .text(p.val + p_time_str);
+
+} // end draw_chart
+
+ // Create a plot TOOLTIP our of the point data
+//debug write this tooltip_html() for acp_web
+function tooltip_html(d)
+{
+    var str = 'DEBUG';
+    //DEBUG this property should be configurable
+    if (d.val) {
+      str += '<br/>Val:'+d.val.toFixed(1);
+    }
+    str += '<br/>Time:' + make_date(d.ts);
+    if (d.journey) //debug get tooltip value from data
+    {
+        str += '<br/>'+JSON.stringify(d.journey).replace(/,/g,', ');
+    }
+    if (d.route_id)
+    {
+        str += '<br/>Route: '+d.route_id; // only if from GTFS feedhandler
+    }
+    return str;
+}
+
+// ************************************************************************************
+// ************** Date forwards / backwards function             *********************
+// ************************************************************************************
+
+// move page to new date +n days from current date
+function date_shift(n)
+{
+    console.log('date_shift()');
+
+    new_date = new Date(YYYY,MM-1,DD); // as loaded in page template config_ values;
+
+    new_date.setDate(new_date.getDate()+n);
+
+    new_year = new_date.getFullYear();
+    new_month = ("0" + (new_date.getMonth()+1)).slice(-2);
+    new_day = ("0" + new_date.getDate()).slice(-2);
+
+    console.log(new_year+'-'+new_month+'-'+new_day);
+    window.location.href = '?date='+new_year+'-'+new_month+'-'+new_day;
+}
+
+// Return a javascript Date, given EITHER a UTC timestamp or a ISO 8601 datetime string
+function make_date(ts)
+{
+    var t;
+    if (isFinite(ts))
+    {
+        t = new Date(ts*1000);
+    }
+    else
+    {
+        // replace anything but numbers by spaces
+        var dt = ts.replace(/\D/g," ");
+
+        // trim any hanging white space
+        dt = dt.replace(/\s+$/,"");
+
+        // split on space
+        var dtcomps = dt.split(" ");
+
+        // modify month between 1 based ISO 8601 and zero based Date
+        dtcomps[1]--;
+
+        t = new Date(Date.UTC(dtcomps[0],dtcomps[1],dtcomps[2],dtcomps[3],dtcomps[4],dtcomps[5]));
+    }
+    return t;
 }
