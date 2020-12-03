60b230964f32f2911543a87de3c4fde6f7d23646
Ian Lewis <ijl20@cam.ac.uk>
2020-12-03 18:04:25 +0000

    ijl20-sql-data-management WIP (created some stub pages)

--- cdbb/data_management/views.py
+++ cdbb/data_management/views.py
@@ -4,7 +4,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
 
 class DMHomeView(TemplateView):
-    template_name = 'data_management/sensor_home.html'
+    template_name = 'data_management/home.html'
+
+###############################################################
+###############################################################
+#             SENSORS                                         #
+###############################################################
+###############################################################
 
 class DMSensorView(LoginRequiredMixin, TemplateView):
     template_name = 'data_management/sensor.html'
@@ -17,14 +23,25 @@ class DMSensorView(LoginRequiredMixin, TemplateView):
 
             return context
 
-class DMSensorTypeView(LoginRequiredMixin, TemplateView):
-    template_name = 'data_management/sensor_type.html'
+class DMSensorEditView(LoginRequiredMixin, TemplateView):
+    template_name = 'data_management/sensor_edit.html'
 
     def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
             context['API_BIM'] = settings.API_BIM
             context['API_SENSORS'] = settings.API_SENSORS
-            context['ACP_TYPE_ID'] = self.kwargs['acp_type_id']
+            context['ACP_ID'] = self.kwargs['acp_id']
+
+            return context
+
+class DMSensorHistoryView(LoginRequiredMixin, TemplateView):
+    template_name = 'data_management/sensor_history.html'
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['API_BIM'] = settings.API_BIM
+            context['API_SENSORS'] = settings.API_SENSORS
+            context['ACP_ID'] = self.kwargs['acp_id']
 
             return context
 
@@ -42,6 +59,45 @@ class DMSensorListView(LoginRequiredMixin, TemplateView):
                 context['FEATURE'] = selected_feature
             else:
                 print("DMSensorListView no feature",kwargs)
+
+            return context
+
+###############################################################
+###############################################################
+#             SENSOR TYPES                                    #
+###############################################################
+###############################################################
+
+class DMSensorTypeView(LoginRequiredMixin, TemplateView):
+    template_name = 'data_management/sensor_type.html'
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['API_BIM'] = settings.API_BIM
+            context['API_SENSORS'] = settings.API_SENSORS
+            context['ACP_TYPE_ID'] = self.kwargs['acp_type_id']
+
+            return context
+
+class DMSensorTypeHistoryView(LoginRequiredMixin, TemplateView):
+    template_name = 'data_management/sensor_type_history.html'
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['API_BIM'] = settings.API_BIM
+            context['API_SENSORS'] = settings.API_SENSORS
+            context['ACP_TYPE_ID'] = self.kwargs['acp_type_id']
+
+            return context
+
+class DMSensorTypeEditView(LoginRequiredMixin, TemplateView):
+    template_name = 'data_management/sensor_type_edit.html'
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['API_BIM'] = settings.API_BIM
+            context['API_SENSORS'] = settings.API_SENSORS
+            context['ACP_TYPE_ID'] = self.kwargs['acp_type_id']
 
             return context
 
