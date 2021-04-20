65d610790d75ce1ea68582d77670da459bb44515
jb2328 <jb2328@cam.ac.uk>
2021-04-20 17:11:22 +0100

    rtconnect - merged with the master file to make use of RTMONITOR_URI splash - newest version with color coding + legends splash_template - added RTMONITOR_URI urls.py and views.py - added SplashView

--- cdbb/space/urls.py
+++ cdbb/space/urls.py
@@ -1,7 +1,7 @@
 # space/urls.py
 from django.urls import path
 
-from .views import HomeView, MapView, RainHomeView, BuildingView, FloorView, FloorspaceView, RainView
+from .views import HomeView, MapView, RainHomeView, BuildingView, FloorView, FloorspaceView, RainView, SplashView
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='space_home'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('building/<crate_id>/', BuildingView.as_view(), name='space_building'),
     path('floor/<crate_id>/', FloorView.as_view(), name='space_floor'),
     path('floorspace/<crate_id>/', FloorspaceView.as_view(), name='space_floorspace'),
-    path('floor_rain/<crate_id>/', RainView.as_view(), name='space_rain')
+    path('floor_rain/<crate_id>/', RainView.as_view(), name='space_rain'),
+    path('floor_splash/<crate_id>/', SplashView.as_view(), name='space_splash')
 ]
